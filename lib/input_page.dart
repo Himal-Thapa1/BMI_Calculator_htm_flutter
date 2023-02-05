ee477fca6ec056b94a62e886fc5e23eb0c4abc86
Himal-Thapa1 <himalthapa2020@outlook.com>
2023-02-05 10:37:35 +0545

    added empty panels

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -9,16 +9,73 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('BMI CALCULATOR')),
-      ),
-      body: Container(
-        margin: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: Color(0xFF1D1E33),
-          borderRadius: BorderRadius.circular(10),
+        appBar: AppBar(
+          title: Center(child: Text('BMI CALCULATOR')),
         ),
-        )
-    );
+        body: Column(
+          children: <Widget>[
+            Expanded(
+                child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      // color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(0xFF1D1E33),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      // color: Color(0xFF1D1E33),
+                      color: Color(0xFF1D1E33),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ],
+            )),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  // color: Color(0xFF1D1E33),
+                  color:Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        // color: Color(0xFF1D1E33),
+                        color: Color(0xFF1D1E33),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        // color: Color(0xFF1D1E33),
+                        color: Color(0xFF1D1E33),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
