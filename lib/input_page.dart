de79c7ac8cf7c2ee76d419789c762e7c7fadd7e5
Himal-Thapa1 <himalthapa2020@outlook.com>
2023-02-05 10:08:02 +0545

    added one container for data to go

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,13 +12,13 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Center(child: Text('BMI CALCULATOR')),
       ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
+      body: Container(
+        margin: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Color(0xFF1D1E33),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        )
     );
   }
 }
