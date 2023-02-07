166843ca520a91aa45339c429c2a7690d73553dd
Himal-Thapa1 <himalthapa2020@outlook.com>
2023-02-07 17:59:04 +0545

    refactored the boilerplate box in the page.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -18,64 +18,45 @@ class _InputPageState extends State<InputPage> {
                 child: Row(
               children: <Widget>[
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      // color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color(0xFF1D1E33),
-                    ),
-                  ),
+                  child: ReusableCard(rang: Color(0xFF1D1E33),),
                 ),
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      // color: Color(0xFF1D1E33),
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
+                  child: ReusableCard(rang: Color(0xFF1D1E33),),
                 ),
               ],
             )),
             Expanded(
-              child: Container(
-                margin: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  // color: Color(0xFF1D1E33),
-                  color:Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
+              child:ReusableCard(rang: Color(0xFF1D1E33),),
             ),
             Expanded(
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        // color: Color(0xFF1D1E33),
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
+                    child: ReusableCard(rang: Color(0xFF1D1E33),),
                   ),
                   Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        // color: Color(0xFF1D1E33),
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
+                    child: ReusableCard(rang: Color(0xFF1D1E33),),
                   ),
                 ],
               ),
             ),
           ],
         ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({required this.rang});
+  final Color rang;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        // color: Color(0xFF1D1E33),
+        borderRadius: BorderRadius.circular(10),
+        color: rang,
+      ),
+    );
   }
 }
