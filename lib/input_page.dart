e9ecdfd1d1c43efe858e4ed1864354185ac9096c
Himal-Thapa1 <himalthapa2020@outlook.com>
2023-03-02 21:16:59 +0545

    added fonstawesome to app module

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
   @override
@@ -18,40 +22,70 @@ class _InputPageState extends State<InputPage> {
                 child: Row(
               children: <Widget>[
                 Expanded(
-                  child: ReusableCard(rang: Color(0xFF1D1E33),),
+                  child: ReusableCard(rang: activeCardColor,
+                  cardChild: Column(
+                    children: <Widget>[
+                      Icon(
+                        FontAwesomeIcons.mars,
+                        size: 80.0,
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Text("MALE",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Color(0xFF8D8E98),
+                      ),)
+                    ],
+                  ),
+                  ),
+
                 ),
                 Expanded(
-                  child: ReusableCard(rang: Color(0xFF1D1E33),),
+                  child: ReusableCard(rang: activeCardColor,
+                  cardChild: Text("Hello there"),),
                 ),
               ],
             )),
             Expanded(
-              child:ReusableCard(rang: Color(0xFF1D1E33),),
+              child:ReusableCard(rang: activeCardColor,
+              cardChild: Text("Hello there"),),
             ),
             Expanded(
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: ReusableCard(rang: Color(0xFF1D1E33),),
+                    child: ReusableCard(rang: activeCardColor,
+                    cardChild: Text("Hello there"),),
                   ),
                   Expanded(
-                    child: ReusableCard(rang: Color(0xFF1D1E33),),
+                    child: ReusableCard(rang: activeCardColor,
+                    cardChild: Text("Hello there"),),
                   ),
                 ],
               ),
             ),
+            Container(
+              color: bottomContainerColor,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity, //this code changes the bottom width to devices max device width.
+              height: bottomContainerHeight,
+            )
           ],
         ));
   }
 }
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.rang});
+  ReusableCard({required this.rang, required this.cardChild});
   final Color rang;
+  final Widget cardChild;
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(16),
+      child: cardChild,
+      margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         // color: Color(0xFF1D1E33),
         borderRadius: BorderRadius.circular(10),
